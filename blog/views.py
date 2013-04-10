9fdfca0ab4005f674054feb48bb2fd1963ac2ebd
Breno Teixeira <brenoteixeira@interlegis.leg.br>
2013-04-10 10:12:18 -0300

    implementação da pagina e rotina de contato

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.mail import send_mail
+from django import forms
 
 from blog.models import Article
 
@@ -8,3 +10,32 @@ def article(request, article_id):
 	article = Article.objects.get(id=article_id)
 	return render_to_response('blog/article.html', locals(),
 		context_instance=RequestContext(request))
+
+def contact(request):
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			form.send_email()
+			success_msg = 'Contato enviado!'
+	else:
+		form = ContactForm()
+
+	return render_to_response('blog/contact.html', locals(),
+		context_instance=RequestContext(request))
+
+class ContactForm(forms.Form):
+	name = forms.CharField(max_length=100)
+	sender = forms.EmailField(required=False)
+	subject = forms.CharField(max_length=100)
+	message = forms.Field(widget=forms.Textarea)
+
+	def send_email(self):
+		subject = self.cleaned_data['subject']
+		from_email = self.cleaned_data['sender']
+		message = """
+			Nome: %(name)s
+			E-mail: %(sender)s
+			Mensagem: %(message)s
+		""" % self.cleaned_data
+
+		send_mail(subject, message, from_email, ['brenotx@gmail.com'],)
--- meu_blog/settings.py
+++ meu_blog/settings.py
@@ -161,3 +161,8 @@ LOGGING = {
         },
     }
 }
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'brenotx@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
